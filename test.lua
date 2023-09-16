56e7ab5c2e4c19896784f47639044989f82ddec6
skyera <zgliu2@gmail.com>
2023-09-15 23:40:16 -0700

    add more test case

--- test.lua
+++ test.lua
@@ -2,4 +2,13 @@ describe("a test", function()
     it("true", function()
         assert.True(1==1)
     end)
+
+    it("yeah", function()
+        assert.truthy("Yup")
+    end)
+
+    it("lots", function()
+        assert.are.same({table="great"}, {table="great"})
+        assert.True(1==2)
+    end)
 end)
