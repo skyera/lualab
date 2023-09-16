e347a3e107fdbefb473a06015d498315805eb678
skyera <zgliu2@gmail.com>
2023-09-16 00:01:22 -0700

    add

--- test.lua
+++ test.lua
@@ -1,4 +1,4 @@
-describe("a test", function()
+describe("my_test", function()
     it("true", function()
         assert.True(1==1)
     end)
@@ -11,4 +11,43 @@ describe("a test", function()
         assert.are.same({table="great"}, {table="great"})
         assert.True(1==2)
     end)
+
+    it("start", function()
+        print("Hello World")
+        print("10!=", fact(10))
+        print(math.pi/4)
+        a = 15
+        print(a^2)
+        n = norm(3.4, 1.0)
+        print(n)
+        print(twice(n))
+    end)
+
+    it("type", function()
+        print(type(true))
+        print(type(io.stdin))
+        print(type({}))
+    end)
+
+    it("args", function()
+        for k, v in pairs(arg) do
+            print(k, v)
+        end
+    end)
 end)
+
+function fact(n)
+    if n == 0 then
+        return 1
+    else
+        return n * fact(n-1)
+    end
+end
+
+function norm(x, y)
+    return math.sqrt(x^2 + y ^2)
+end
+
+function twice(x)
+    return 2.0 * x
+end
