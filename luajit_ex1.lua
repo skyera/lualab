2cf55e850d057d912d5e014fed0ce6054fba1539
Esther Liu <s233419@sjusd.org>
2024-09-07 23:50:17 -0700

    use atoi

--- luajit_ex1.lua
+++ luajit_ex1.lua
@@ -2,9 +2,13 @@ local ffi = require("ffi")
 
 ffi.cdef[[
 int rand(void);
+int atoi(const char*);
 ]]
 
 for i = 0, 3 do
     local num = ffi.C.rand()
     print(num)
 end
+
+local num = ffi.C.atoi("12")
+print(num)
