1e60d847ae981caa4d5e103b4884df5ec66d67b3
Esther Liu <s233419@sjusd.org>
2024-09-08 07:31:36 -0700

    add printf

--- luajit_ex1.lua
+++ luajit_ex1.lua
@@ -3,6 +3,7 @@ local ffi = require("ffi")
 ffi.cdef[[
 int rand(void);
 int atoi(const char*);
+int printf(const char* fmt, ...);
 ]]
 
 for i = 0, 3 do
@@ -12,3 +13,4 @@ end
 
 local num = ffi.C.atoi("12")
 print(num)
+print("Hello, %s\n", "world")
