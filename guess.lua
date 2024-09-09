259ba788eeb83deb9861cae46a1f6a4898bf0336
zliu <zgliu2@gmail.com>
2024-09-08 18:36:22 -0700

    add 2nd version of class

--- guess.lua
+++ guess.lua
@@ -2,11 +2,13 @@ math.randomseed(os.time())
 number = math.random(1, 100)
 player = {}
 player.guess = 0
+player.count = 0
 
 while player.guess ~= number do
     print("Guess a number between 1 and 100")
     player.answer = io.read()
     player.guess = tonumber(player.answer)
+    player.count = player.count + 1
 
     if player.guess > number then
         print("Too high")
@@ -14,6 +16,8 @@ while player.guess ~= number do
         print("Too low")
     else
         print("That's right!")
-        os.exit()
+        -- os.exit()
     end
 end
+
+io.write("You got it right after ", player.count, " tries\n")
--- oo.lua
+++ oo.lua
@@ -1,5 +1,10 @@
+-- http://lua-users.org/wiki/ObjectOrientationTutorial
+-- : syntax sugar
+--      call function
+--      insert table as self
 local MyClass = {}
 MyClass.__index = MyClass
+print("MyClass ", MyClass)
 
 function MyClass.new(value)
     local obj = setmetatable({}, MyClass)
@@ -21,4 +26,34 @@ local obj = MyClass.new(3)
 print("obj ", obj)
 print(obj:get_value())
 obj:set_value(4)
+print(obj:get_value())
+
+--
+print("2nd version of class")
+local MyClass = {}
+MyClass.__index = MyClass
+
+setmetatable(MyClass, {
+    __call = function(cls, ...)
+        return cls.new(...)
+    end,
+})
+
+function MyClass.new(value)
+    local obj = setmetatable({}, MyClass)
+    obj.value = value
+    return obj
+end
+
+function MyClass:set_value(newval)
+    self.value = newval
+end
+
+function MyClass:get_value()
+    return self.value
+end
+
+local obj = MyClass(3)
+print(obj:get_value())
+obj:set_value(1)
 print(obj:get_value())
