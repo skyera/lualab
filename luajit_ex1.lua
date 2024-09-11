0de7bcc6fbc219cbfa77331d68c22f8b3d945511
Zhigang Liu <zliu@Air.local>
2024-09-10 23:01:09 -0700

    Add struct

--- luajit_ex1.lua
+++ luajit_ex1.lua
@@ -4,6 +4,11 @@ ffi.cdef[[
 int rand(void);
 int atoi(const char*);
 int printf(const char* fmt, ...);
+
+typedef struct {
+    int x;
+    int y;
+} Point;
 ]]
 
 for i = 0, 3 do
@@ -14,3 +19,8 @@ end
 local num = ffi.C.atoi("12")
 print(num)
 ffi.C.printf("Hello, %s\n", "world")
+
+local p = ffi.new("Point")
+p.x = 10
+p.y = 20
+print(p.x, p.y)
