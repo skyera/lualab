fb3894a817e1878bcb419f944468bcf4a113c459
skyera <zgliu2@gmail.com>
2024-09-13 12:42:30 -0700

    use C struct

--- luajit_ex1.lua
+++ luajit_ex1.lua
@@ -24,3 +24,63 @@ local p = ffi.new("Point")
 p.x = 10
 p.y = 20
 print(p.x, p.y)
+
+--
+local floor = math.floor
+
+local function image_ramp_green(n)
+    local img = {}
+    local f = 255/(n-1)
+
+    for i=1,n do
+        img[i] = {red=0, green=floor((i-1)*f), blue=0, alpha=255}
+    end
+
+    return img
+end
+
+local function image_to_gray(img, n)
+    for i=1,n do
+        local y=floor(0.3*img[i].red + 0.59*img[i].green + 0.11*img[i].blue)
+        img[i].red = y
+        img[i].green = y
+        img[i].blue = y
+    end
+end
+
+local N = 400*400
+local img = image_ramp_green(N)
+for i=1, 1000 do
+    image_to_gray(img, N)
+end
+
+-- ffi: use C data structure
+print("ffi")
+ffi.cdef[[
+typedef struct { uint8_t red, green, blue, alpha; } rgba_pixel;
+]]
+
+local function image_ramp_green(n)
+    local img = ffi.new("rgba_pixel[?]", n)
+    local f=255/(n-1)
+    for i=0,n-1 do
+        img[i].green=i*f
+        img[i].alpha=255
+    end
+    return img
+end
+
+local function image_to_gray(img, n)
+    for i=0, n-1 do
+        local y = 0.3*img[i].red + 0.59*img[i].green + 0.11*img[i].blue
+        img[i].red = y
+        img[i].green = y
+        img[i].blue = y
+    end
+end
+
+local N = 400*400
+local img = image_ramp_green(N)
+for i=0, 1000 do
+    image_to_gray(img, N)
+end
