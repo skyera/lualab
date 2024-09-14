0b073e6522d6c41c7b5aa25d5353a7871e3dff2d
zliu <zgliu2@gmail.com>
2024-09-14 16:37:45 -0700

    use object

--- script.lua
+++ script.lua
@@ -1,3 +1,37 @@
+Car = {}
+
+function Car:new(make, model, year)
+    local obj = {}
+    setmetatable(obj, self)
+    self.__index = self
+
+    obj.make = make
+    obj.model = model
+    obj.year = year
+    obj.speed = 0
+    return obj
+end
+
+function Car:accelerate(increase)
+    self.speed = self.speed + increase
+    print(self.make .. " " .. self.model .. " " .. " is now going at "
+        .. self.speed)
+end
+
+function Car:brake(decrease)
+    self.speed = self.speed - decrease
+    if self.speed < 0 then
+        self.speed = 0
+    end
+    print(self.make .. " " .. self.model .. " " .. " slowed down to " 
+        .. self.speed)
+end
+
+function Car:getDetails()
+    return "Make: " .. self.make .. ", Model:  " .. self.model .. ", Year: " 
+    .. self.year
+end
+
 local function print_help()
     print("Usage: lua script.lua <arg1> <arg2>")
     print("Description:")
@@ -21,3 +55,8 @@ print("Argument 2:", arg2)
 for i=0, 10 do
     print("[] Item", i+1)
 end
+
+myCar = Car:new("Ford", "Mustang", 1969)
+myCar:accelerate(20)
+myCar:brake(5)
+print(myCar:getDetails())
