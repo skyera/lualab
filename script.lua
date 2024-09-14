a289fe3c2214316dd29f5ecc0e19948a09286f01
skyera <zgliu2@gmail.com>
2024-09-14 11:03:50 -0700

    add arg

---
+++ script.lua
@@ -0,0 +1,19 @@
+local function print_help()
+    print("Usage: lua script.lua <arg1> <arg2>")
+    print("Description:")
+    print(" <arg1>: First arg")
+    print(" <arg2>: Second arg")
+    print("Example:")
+    print(" lua script.lya input1 input2")
+end
+
+
+if #arg < 2 or arg[1] == "-h" or arg[1] == "--help" then
+    print_help()
+    os.exit(1)
+end
+
+local arg1 = arg[1]
+local arg2 = arg[2]
+print("Argument 1:", arg1)
+print("Argument 2:", arg2)
