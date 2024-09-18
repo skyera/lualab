544c7e4df1e8f034a17c6ccbbcbf81f9553e7a13
skyera <zgliu2@gmail.com>
2024-09-18 08:09:07 -0700

    update

--- embed1.cpp
+++ embed1.cpp
@@ -8,7 +8,26 @@ extern "C" {
 int main(int argc, char** argv) {
     lua_State* L = luaL_newstate();
     luaL_openlibs(L);
+    
+    if (argc == 2) {
+        char* filename = argv[1];
+        int result = luaL_loadfile(L, filename);
 
+        if (result != 0) {
+            printf("Could not load %s\n", filename);
+            lua_close(L);
+            return -1;
+        }
+
+        result = lua_pcall(L, 0, 0, 0);
+        if (result != 0) {
+            const char* error = lua_tostring(L, -1);
+            printf("Error %s\n", error);
+            lua_close(L);
+            return -1;;
+        }
+    }
+    printf("Done\n");
     lua_close(L);
     return 0;
 }
