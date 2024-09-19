494bdef453ea88cdbcd221df03b92d0b4d396b1b
skyera <zgliu2@gmail.com>
2024-09-18 22:39:51 -0700

    json

--- create.lua
+++ create.lua
@@ -1,3 +1,17 @@
+local json = require 'json'
+
+local t = json.decode('[1,2,3,{"x":10}]') -- Returns { 1, 2, 3, { x = 10 } }
+
+for k, v in pairs(t) do
+    print(k, v)
+end
+t2 = t[4]
+
+print(t2.x)
+for k, v in pairs(t2) do
+    print(k, v)
+end
+
 local tbl = foo.create_table()
 print(tbl)
 
