9a1fad595bada8eb2254b94a654d87252a23ed54
skyera <zgliu2@gmail.com>
2024-09-18 21:46:01 -0700

    return table in c++ and use it in Lua

---
+++ create.lua
@@ -0,0 +1,8 @@
+local tbl = foo.create_table()
+print(tbl)
+
+for k, v in pairs(tbl) do
+    print(k, v)
+end
+local x = foo.bar()
+print(x)
--- embed1.cpp
+++ embed1.cpp
@@ -1,13 +1,41 @@
 // build: make embed1.cpp -o embed1 -I/path/include /path/libluajit.a -ldl
+
+// compile: 
+// g++ embed1.cpp -o embed1 -I/home/pi/test/luajit/src/ ~/test/luajit/src/libluajit.a  -ldl
+
+// export CPLUS_INCLUDE_PATH="/home/pi/test/LuaBridge/Source/LuaBridge:/home/pi/test/LuaBridge/Source:$CPLUS_INCLUDE_PATH"
+
 extern "C" {
 #include "lua.h"
 #include "lauxlib.h"
 #include "lualib.h"
 }
 
+#include "LuaBridge.h"
+using namespace luabridge;
+        
+LuaRef CreateTable(lua_State* L) {
+    LuaRef table = LuaRef::newTable(L);
+
+    table["name"] = std::string("Lua");
+    table["version"] = 1.0;
+    table["active"] = true;
+    return table;
+}
+
+int bar_func() {
+	return 2;
+}
+
 int main(int argc, char** argv) {
     lua_State* L = luaL_newstate();
     luaL_openlibs(L);
+
+    getGlobalNamespace(L)
+        .beginNamespace("foo")
+        .addFunction("create_table", CreateTable)
+        .addFunction("bar", bar_func)
+        .endNamespace();
     
     if (argc == 2) {
         char* filename = argv[1];
