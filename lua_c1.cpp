fc6f52d8e55bb8ab9dd7664be45361c03ada489a
skyera <zgliu2@gmail.com>
2024-09-22 08:23:05 -0700

    Lua read C vars

--- lua_c1.cpp
+++ lua_c1.cpp
@@ -11,35 +11,59 @@ extern "C" {
 int main(int argc, char** argv) {
     lua_State* L = luaL_newstate();
     luaL_openlibs(L);
+    
+    {
+        // C read lua vars
+        char code[] = "class='Warrior';attack=56;defense=43";
 
-    char code[] = "class='Warrior';attack=56;defense=43";
+        int result = luaL_dostring(L, code);
+        if (result != 0 ) {
+            const char* error = lua_tostring(L, -1);
+            printf("cannot dostring %s\n", error);
+            lua_pop(L, 1);
+            lua_close(L);
+            return -1;
+        }
 
-    int result = luaL_dostring(L, code);
-    if (result != 0 ) {
-        const char* error = lua_tostring(L, -1);
-        printf("cannot dostring %s\n", error);
-        lua_pop(L, 1);
-        lua_close(L);
-        return -1;
+        int stack_base = lua_gettop(L);
+
+        // push values on stack
+        lua_getglobal(L, "class");
+        lua_getglobal(L, "attack");
+        lua_getglobal(L, "defense");
+
+        // read values on stack
+        const char* class_p = lua_tostring(L, stack_base + 1);
+        char class_sz[32];
+        strcpy(class_sz, class_p);
+        int attack = lua_tointeger(L, stack_base + 2);
+        int defense = lua_tointeger(L, stack_base + 3);
+
+        // clean up stack
+        lua_pop(L, 3);  
+        printf("class=%s;attack=%d;defense=%d\n", class_sz, attack, defense);
     }
 
-    int stack_base = lua_gettop(L);
+    {
+        // create Lua vars in C
+        lua_pushstring(L, "Warrior");
+        lua_pushnumber(L, 56);
+        lua_pushnumber(L, 43);
 
-    // push values on stack
-    lua_getglobal(L, "class");
-    lua_getglobal(L, "attack");
-    lua_getglobal(L, "defense");
-    
-    // read values on stack
-    const char* class_p = lua_tostring(L, stack_base + 1);
-    char class_sz[32];
-    strcpy(class_sz, class_p);
-    int attack = lua_tointeger(L, stack_base + 2);
-    int defense = lua_tointeger(L, stack_base + 3);
-    
-    // clean up stack
-    lua_pop(L, 3);  
-    printf("class=%s;attack=%d;defense=%d\n", class_sz, attack, defense);
+        lua_setglobal(L, "defense");
+        lua_setglobal(L, "attack");
+        lua_setglobal(L, "class");
+
+        char code[] = "print('class ' .. class .. ' attack ' .. attack .. ' defense ' .. defense)";
+        int result = luaL_dostring(L, code);
+        if (result != 0) {
+            const char* error = lua_tostring(L, -1);
+            printf("error %s\n", error);
+            lua_pop(L, lua_gettop(L));
+            lua_close(L);
+            return -1;
+        }
+    }
 
     lua_close(L);
     return 0;
