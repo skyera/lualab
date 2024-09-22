ac5c5c039a29e1975b1beb24880f1c1c4e61dfc0
skyera <zliu@Air.local>
2024-09-21 23:23:17 -0700

    add C read lua vars

---
+++ lua_c1.cpp
@@ -0,0 +1,40 @@
+extern "C" {
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+}
+#include <string.h>
+
+int main(int argc, char** argv) {
+    lua_State* L = luaL_newstate();
+    luaL_openlibs(L);
+
+    char code[] = "class='Warrior';attack=56;defense=43";
+
+    int result = luaL_dostring(L, code);
+    if (result != 0 ) {
+        const char* error = lua_tostring(L, -1);
+        printf("cannot dostring %s\n", error);
+        lua_pop(L, 1);
+        lua_close(L);
+        return -1;
+    }
+
+    int stack_base = lua_gettop(L);
+
+    lua_getglobal(L, "class");
+    lua_getglobal(L, "attack");
+    lua_getglobal(L, "defense");
+
+    const char* class_p = lua_tostring(L, stack_base + 1);
+    char class_sz[32];
+    strcpy(class_sz, class_p);
+    int attack = lua_tointeger(L, stack_base + 2);
+    int defense = lua_tointeger(L, stack_base + 3);
+
+    lua_pop(L, 3);
+    printf("class=%s;attack=%d;defense=%d\n", class_sz, attack, defense);
+
+    lua_close(L);
+    return 0;
+}
