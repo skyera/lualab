6bf52427d8a8f796ad05e8a7f5622f6c490a74ee
skyera <zgliu2@gmail.com>
2024-10-07 08:16:43 -0700

    update

--- tut.lua
+++ tut.lua
@@ -46,3 +46,47 @@ print(x)
 
 x, y = string.find("hello Lua user", "Lua")
 print(x, y)
+
+list = {{3}, {5}, {2}, {-1}}
+table.sort(list, function(a, b) return a[1] < b[1] end)
+for i, v in ipairs(list) do
+    print(v[1])
+end
+
+-- variable args
+f = function(x, ...)
+    x(...)
+end
+
+f(print, "1 2 3", "abc")
+
+-- select
+f = function(...)
+    print(select('#', ...))
+    print(select(3, ...))
+end
+f(1,2,3,4,5)
+
+-- ...->table
+f = function(...)
+    tbl = {...}
+    print(_VERSION)
+    -- print(table.unpack(tbl))
+end
+f("a", "b", "c")
+
+a = {b={}}
+function a.b.f(...)
+    print(...)
+end
+a.b.f(1,2,3)
+
+-- closure
+local x = 5
+local function f()
+    print(x)
+end
+
+f()
+x = 6
+f()
