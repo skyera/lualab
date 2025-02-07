7f9d532e3f4a474386335e3632de2546037e05e1
skyera <zgliu2@gmail.com>
2025-02-06 21:11:37 -0800

    update

--- todo.lua
+++ todo.lua
@@ -19,6 +19,26 @@ end
 function main()
     print("My todo list")
     local tasks = load_tasks()
+
+    while true do
+        print("1. Add Task")
+        print("2. View Tasks")
+        print("3. Quit")
+        local choice = tonumber(io.read())
+        if choice == 1 then
+            print("Enter task description:")
+            local task = io.read()
+            table.insert(tasks, task)
+            save_tasks(tasks)
+            print("Task added!")
+        elseif choice == 2 then
+            display_tasks(tasks)
+        elseif choice == 3 then
+            break
+        else
+            print("Invalid choice!")
+        end
+    end
 end
 
 main()
