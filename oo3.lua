1b72dacddccc384ab72fe9a6b384e1447fdb37cf
zliu <zgliu2@gmail.com>
2025-09-09 06:37:39 -0700

    class

--- oo3.lua
+++ oo3.lua
@@ -1,8 +1,13 @@
 Animal = {}
 Animal.__index = Animal
 
+<<<<<<< HEAD
 function Animal:new(name)
     o = setmetatable({}, self)
+=======
+function Animal.new(name)
+    local o = setmetatable({}, Animal)
+>>>>>>> 669d474 (class Animal)
     o.name = name
     o.sound = "Unknown"
     return o
@@ -14,11 +19,19 @@ end
 
 Dog = {}
 Dog.__index = Dog
+<<<<<<< HEAD
 
 function Dog:new(name)
     o = Animal:new(name)
     o.sound = "Woof!"
     setmetatable(o, self)
+=======
+setmetatable(Dog, {__index = Animal})
+
+function Dog.new(name)
+    local o = setmetatable(Animal.new(name), Dog)
+    o.sound = "Woof!"
+>>>>>>> 669d474 (class Animal)
     return o
 end
 
@@ -26,7 +39,12 @@ function Dog:makeSound()
     print(self.name .. " says " .. self.sound)
 end
 
+<<<<<<< HEAD
 local animal = Animal:new("Animal")
 local dog = Dog:new("Dog")
+=======
+local animal = Animal.new("Animal")
+local dog = Dog.new("Dog")
+>>>>>>> 669d474 (class Animal)
 animal:makeSound()
 dog:makeSound()
