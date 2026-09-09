ad0e498503d19b05a3e818b124a7c0091e357301
zliu <zgliu2@gmail.com>
2026-09-09 07:55:32 -0700

    Use getline in embed_repl_simple.cpp to handle arbitrary line lengths
    - Replace fixed 256-byte stack buffer with POSIX getline
    - Prevent line truncation and syntax errors on long input statements
    - Free allocated line buffer before program exit

--- embed_repl_simple.cpp
+++ embed_repl_simple.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 extern "C" {
 #include "lua.h"
@@ -7,18 +8,21 @@ extern "C" {
 }
 
 int main(void) {
-    char buff[256];
+    char *line = NULL;
+    size_t len = 0;
     int error;
     lua_State *L = luaL_newstate();
     luaL_openlibs(L);
 
-    while (fgets(buff, sizeof(buff), stdin) != NULL) {
-        error = luaL_loadstring(L, buff) || lua_pcall(L, 0, 0, 0);
+    while (getline(&line, &len, stdin) != -1) {
+        error = luaL_loadstring(L, line) || lua_pcall(L, 0, 0, 0);
         if (error) {
             fprintf(stderr, "%s\n", lua_tostring(L, -1));
             lua_pop(L, 1);
         }
     }
+    free(line);
     lua_close(L);
     return 0;
 }
+
